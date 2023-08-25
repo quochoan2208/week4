23613720ccc463871c087f3be2abe2c34603120c
quochoan2208 <quochoan.hoang@griffithuni.edu.au>
2023-08-25 21:51:10 +1000

    task2 completed

--- server/index.js
+++ server/index.js
@@ -11,16 +11,38 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + "/www/form.html");
     // res.send('This is the authentication route.');
 });
-app.get('/api/auth', (req, res) => {
-    res.send('This is the authentication route.');
+const users = [
+  { email: 'abc@gmail.com.au', birthday: '12/08/2000', age: 18, pwd: '123',valid: true  },
+  { email: 'zyz@gmail.com.au', birthday: '11/05/2001', age: 28, pwd: '262',valid: false  },
+  { email: 'abc@gmail.com.au', birthday: '19/02/2003', age: 38, pwd: '356',valid: true  },
+];
+
+
+// Chuyển đổi đối tượng đã được chỉnh sửa thành chuỗi JSON
+
+  app.get('/api/auth', (req, res) => {
+    const submittedUsername = req.query.username;
+    const submittedPassword = req.query.password;
+  
+    const user = users.find(u => u.username === submittedUsername && u.password === submittedPassword);
+  
+    if (user) {
+      const userData = {
+        username: user.username,
+        birthdate: user.birthdate,
+        age: user.age,
+        email: user.email,
+        valid: true
+      };
+      res.json(userData);
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
+    }
   });
   
+  
 app.post('/api/login', function (req, res) {
-    let users = [
-        { email: 'abc@gmail.com.au', birthday: '12/08/2000', age: 18, pwd: '123',valid: true  },
-        { email: 'zyz@gmail.com.au', birthday: '11/05/2001', age: 28, pwd: '262',valid: false  },
-        { email: 'abc@gmail.com.au', birthday: '19/02/2003', age: 38, pwd: '356',valid: true  },
-    ];
+    
     if (!req.body) {
         return res.sendStatus(400);
     }
@@ -29,8 +51,14 @@ app.post('/api/login', function (req, res) {
     customer.upwd = req.body.upwd;
     customer.valid = false;
     for (let i = 0; i < users.length; i++) {
-        if (req.body.email === users[i].email && req.body.upwd === users[i].pwd) {
+        if (req.body.email === users[i].email && req.body.upwd === users[i].pwd && users[i].valid == true) {
             customer.valid = true;
+            const userWithoutPwd = { ...users[i] };
+            delete userWithoutPwd.pwd;
+            let userString = JSON.stringify(userWithoutPwd);
+            console.log(userString);
+
+            
             break;
         }
     }
